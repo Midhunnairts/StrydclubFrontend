5381c269c70cce1186f7fc56afc5370ceb9d6908
midhun Nair T S <midhunnairts@midhuns-MacBook-Air.local>
2026-06-01 19:22:17 +0530

    minor fixes in home page

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './hero.html',
   styleUrl: './hero.scss'
 })
-export class HeroComponent {}
+export class HeroComponent {
+  get isLoggedIn(): boolean {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      return !!localStorage.getItem('token');
+    }
+    return false;
+  }
+}
+
