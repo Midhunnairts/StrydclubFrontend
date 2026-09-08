1ff38ae7115629915f9f93f52b5eb91540f36533
Midhun Nair <midhunnair102@gmail.com>
2026-09-08 22:29:57 +0530

    navbar ui

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://strydclubbackend-1.onrender.com/api',
+  apiUrl: 'http://localhost:3000/api',
   razorpayKeyId: 'rzp_test_TF1mZOVdqrbek8',
   cashfreeEnv: 'sandbox'
 };
