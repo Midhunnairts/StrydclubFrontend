563e702e52a55b3ed3709a809afff403b965776b
Midhun Nair <midhunnair102@gmail.com>
2026-09-19 10:18:38 +0530

    env change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://strydclubbackend-1.onrender.com/api',
   razorpayKeyId: 'rzp_test_TF1mZOVdqrbek8',
-  cashfreeEnv: 'sandbox'
+  cashfreeEnv: 'production'
 };
